ee1a0c1aaa14a307cf353e673245846d1543f653
Steven R. Loomis <srl295@gmail.com>
2022-10-04 12:45:12 -0500

    WIP

--- tools/cldr-apps/js/test/nonbrowser/test-cldrXpathUtils.mjs
+++ tools/cldr-apps/js/test/nonbrowser/test-cldrXpathUtils.mjs
@@ -1,7 +1,6 @@
-const expect = require("chai").expect;
-const mocha = require("mocha");
+import {expect} from "chai";
 
-import * as cldrXpathUtils from "../../src/esm/cldrXpathUtils";
+import * as cldrXpathUtils from "../../src/esm/cldrXpathUtils.js";
 
 describe("cldrXpathUtils test", function () {
   describe("extraPathAllowsNullValue() test", function () {
