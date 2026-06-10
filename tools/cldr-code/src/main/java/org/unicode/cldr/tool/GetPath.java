f04598616ac6d9c90f51fa72cef61f8dcdd59fe2
Steven R. Loomis <srl295@gmail.com>
2026-06-10 16:16:58 -0500

    CLDR-0000 GetPath

---
+++ tools/cldr-code/src/main/java/org/unicode/cldr/tool/GetPath.java
@@ -0,0 +1,40 @@
+package org.unicode.cldr.tool;
+
+import org.unicode.cldr.util.CLDRConfig;
+import org.unicode.cldr.util.CLDRFile;
+import org.unicode.cldr.util.CLDRLocale;
+import org.unicode.cldr.util.CLDRTool;
+import org.unicode.cldr.util.LocaleInheritanceInfo;
+
+@CLDRTool(alias = "get", description="work with paths from cli")
+public class GetPath {
+    public static void main(String args[]) {
+        CLDRLocale l = null;
+        String p = null;
+        for(int i=0; i<args.length; i++) {
+            final String s = args[i];
+            switch(s) {
+                case "--locale":
+                    i++;
+                    l = CLDRLocale.getInstance(args[i]);
+                    break;
+                case "--path":
+                    i++;
+                    p = args[i];
+                case "--explain":
+                    explainPath(l, p);
+                    break;
+            }
+        }
+    }
+
+    private static void explainPath(CLDRLocale l, String p) {
+        System.out.println("XPath: " + p);
+        System.out.println("Loading: " + l);
+        final CLDRFile f = CLDRConfig.getInstance().getCLDRFile(l.getBaseName(), true);
+        for(LocaleInheritanceInfo path : f.getPathsWhereFound(p)) {
+            System.out.println(path.toString());
+        }
+        System.out.println();
+    }
+}
